8d922b5954d9720ea9e333981dce7b38a930f7fb
gustarrafil <gustavotarrafil@gmail.com>
2020-08-19 22:11:48 -0300

    segunda tentativa php

--- contato.php
+++ contato.php
@@ -2,20 +2,36 @@
 if(!isset($_POST['submit'])) {
     echo "error";
 }
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
 
-if(empty($name)||empty($visitor_email)) {
-    echo "preciso saber quem é você...";
-    exit;
+if (isset($_POST['name']) && isset($_POST['email'])) {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $to = "gustavotarrafil@gmail.com";
+    $subject = "alguem chamou no site";
+    $message = $_POST['message'];
+    $body = " ".$message." ";
+    $headers = "From: ".$name." <".$email.">\r\n";
+    $headers .= "Reply-To: ".$email."\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset-utf-8";
+
+    
+
+    if(empty($name)||empty($visitor_email)) {
+        echo "preciso saber quem é você...";
+        exit;
+    }
+    else {
+        $send = mail($to, $subject, $body, $headers);
+        if ($send) {
+            echo "obrigado";
+        } else {
+            echo "erro ao mandar";
+        }
+    }
+
 }
 
-$email_from = "gustavotarrafil@gmail.com";
-$email_subject = "alguem chamou no site";
-$email_body = "voce tem uma mensagem:\nquem mandou: $visitor_email\n mensagem: $message". 
-$to = "gustavotarrafil@gmail.com";
-$headers = "From: Semail_from";
-
-mail($to,$email_subject,$email_body,$headers); ?>
+?>
 <?php  ?>
